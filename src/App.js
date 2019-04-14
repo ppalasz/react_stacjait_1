8ea52bebdf4e725211c9aea6466c88c0c2367dda
Paweł Pałasz <ppalasz@poczta.onet.pl>
2019-04-14 09:51:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+text="empty"
+
+  constructor(props) {
+    super()
+    this.text = props.text
+    this.state = {
+      text: props.text
+ }
+ this.handleClick = this.handleClick.bind(this)
+}
+
+handleClick() {
+  this.setState(prevState => {
+      return{
+        text: prevState.text + "."
+      }
+  })
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div class="spin-link" onClick={this.handleClick}>{this.state.text}</div>
     );
   }
 }
